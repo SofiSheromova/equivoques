020a9dccae5986a4ee2b130268aeb9f50764388d
sofi.sher <sofi.sheromova@gmail.com>
2021-01-13 00:35:57 +0500

    refactor

---
+++ controllers/game-controller.js
@@ -0,0 +1,33 @@
+exports.main = function(req, res, next) {
+  res.render('index', {title: 'Экивоки'});
+};
+
+exports.authorizationForm = function(req, res, next) {
+  res.render('authorization-form', {title: 'Авторизация'});
+};
+
+exports.authorization = function(req, res, next) {
+  const userAge = new Date(
+      new Date() - new Date(Date.parse(req.body.userBirthday)),
+  ).getFullYear() - 1970;
+  res.cookie('userAge', userAge, {maxAge: 900000, httpOnly: true});
+  res.cookie('userName', req.body.userName, {maxAge: 900000, httpOnly: true});
+  res.redirect(req.query.referer || '/');
+};
+
+exports.rulesPage = function(req, res, next) {
+  if (!req.cookies.userAge) {
+    res.redirect('/authorization?referer=/rules');
+    return;
+  }
+  res.render('rules', {title: 'Правила'});
+};
+
+exports.gamePage = function(req, res, next) {
+  console.log(req.cookies);
+  if (!req.cookies.userAge) {
+    res.redirect('/authorization?referer=/game');
+    return;
+  }
+  res.render('game-start', {title: 'Игра'});
+};
--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,17 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+const GameController = require('../controllers/game-controller');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Экивоки'});
-});
+router.get('/', GameController.main);
 
-router.get('/authorization', function(req, res, next) {
-  res.render('authorization-form', {title: 'Авторизация'});
-});
+router.get('/authorization', GameController.authorizationForm);
 
-router.post('/authorization', function(req, res, next) {
-  const userAge = new Date(
-      new Date() - new Date(Date.parse(req.body.userBirthday)),
-  ).getFullYear() - 1970;
-  res.cookie('userAge', userAge, {maxAge: 900000, httpOnly: true});
-  res.cookie('userName', req.body.userName, {maxAge: 900000, httpOnly: true});
-  res.redirect(req.query.referer || '/');
-});
+router.post('/authorization', GameController.authorization);
 
-router.get('/rules', function(req, res, next) {
-  if (!req.cookies.userAge) {
-    res.redirect('/authorization?referer=/rules');
-    return;
-  }
-  res.render('rules', {title: 'Правила'});
-});
+router.get('/rules', GameController.rulesPage);
 
-router.get('/game', function(req, res, next) {
-  console.log(req.cookies);
-  if (!req.cookies.userAge) {
-    res.redirect('/authorization?referer=/game');
-    return;
-  }
-  res.render('game-start', {title: 'Игра'});
-});
+router.get('/game', GameController.gamePage);
 
 module.exports = router;
