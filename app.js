7cfea6607e81299139aed7575323253bbf95a908
sofi.sher <sofi.sheromova@gmail.com>
2021-01-21 22:00:38 +0500

    error handling, error page

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
+if (app.get('env') === 'development') {
+  app.use(logger('dev'));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -49,12 +49,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const message = err.message;
+  const error = req.app.get('env') === 'development' ? err : undefined;
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {message, error});
 });
 
 module.exports = app;
